6c980a4e78b04b31b4e56e2844f3e6e3d03fe664
mim2041 <suraiyamim338@gmail.com>
2023-07-01 10:18:39 +0600

    informations updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,25 +3,24 @@ import profile from '../../assets/images/profile.png'
 
 const About = () => {
     return (
-        <div className='flex text-white' style={{ position: 'relative'}}>
-            <div className='w-1/2'>
-                <img src={profile} alt="" />
-            </div>
-            <div className='w-1/2 flex flex-col justify-center'>
+        <div className='text-center text-white m-8 lg:m-24' style={{ position: 'relative'}}>
+            
+            <div className=' flex flex-col justify-center'>
                 <div>
                 <h1 className='text-3xl'>About Me</h1>
-                <p className='w-3/4'>Hello I am <span>Mim Khatun</span>. I am a student of B.Sc in Information Technology <span>(IT)</span>, Jahangirnagar University. I am a Full Stack Developer. I am a <span>Quick Learner</span>. Passionate and driven developer with a relentless pursuit of excellence in crafting elegant and efficient code solutions. Strong problem-solving skills, hard working capabilities enable me to identify and resolve complex technical challenges efficiently.</p>
+                <p className='text-center lg:w-1/2 mx-auto my-8'>Hello I am <span>Mim Khatun</span>. I am a student of B.Sc in Information Technology <span>(IT)</span>, Jahangirnagar University. I am a Full Stack Developer. I am a <span>Quick Learner</span>. Passionate and driven developer with a relentless pursuit of excellence in crafting elegant and efficient code solutions. Strong problem-solving skills, hard working capabilities enable me to identify and resolve complex technical challenges efficiently.</p>
                 </div>
-                <div className='flex justify-between'>
-                    <div className='border p-8'>
-                        <h1 className='text-3xl font-bold'>30+</h1>
+                <div className='flex gap-10  mx-auto'>
+                    <div className='border p-8 rounded-lg'>
+                        <h1 className='text-3xl font-bold text-pink-800'>30+</h1>
                         <p>Completed Projects</p>
                     </div>
-                    <div className='border p-8'>
-                        <h1 className='text-3xl font-bold'>100+</h1>
+                    <div className='border p-8 rounded-lg'>
+                        <h1 className='text-3xl font-bold text-pink-800'>100+</h1>
                         <p>Problem Solved</p>
                     </div>
                 </div>
+                <button className='flex items-center justify-center gap-2 text-2xl text-white bg-gradient-to-r from-purple-900 to-purple-400 py-2 px-4 rounded-lg lg:w-1/3 mx-auto my-8'>Download Resume</button>
             </div>
         </div>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import profile from '../../assets/images/profile.png';
 import ParticlesJS from '../../components/Particles';
 // import Particles from 'react-tsparticles';
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [text] = useTypewriter({
@@ -22,23 +24,27 @@ const Home = () => {
   }, []);
 
   return (
-    <div style={{ position: 'relative', fontFamily: "'Lobster', cursive" }} className='bg-cover bg-center mt-0 flex'>
+    <div style={{ position: 'relative'}}>
+      <div className='bg-cover bg-center mt-0 flex flex-col lg:flex-row items-center'>
       <div className='particles-container' id='particles-container' style={{ position: 'absolute', top: 0, left: 0, zIndex: -1 }} />
       <div className='w-1/2'>
         <img src={profile} alt='' />
       </div>
-      <div className='m-10 flex flex-col justify-center gap-4' style={{ zIndex: 1 }}>
+      <div style={{fontFamily: "'Lobster', cursive",  zIndex: 1  }} className='m-10 flex flex-col justify-center gap-4'>
         <h2 className='text-3xl text-white'>Hello, This is</h2>
-        <h1 className='text-4xl text-white font-bold'>Mim Khatun</h1>
+        <h1 className='text-4xl text-white font-bold ' style={{linearGradient: "to right, #51087E, #B24BF3",  }}>Mim Khatun</h1>
+        <div>
         <h3 className='text-3xl font-bold text-blue-300'>
-          I am a
           <span>{text}</span>
           <span>
             <Cursor />
           </span>
         </h3>
-        <button className='text-2xl text-white bg-gradient-to-r'>Want to know more about me?</button>
+        </div>
+        <p className='text-white lg:w-1/2'>Passionate for exploring new things. Addicted for coding and building outstanding websites. From building scalable web applications to designing intuitive user interfaces, I am adept at transforming complex ideas into practical solutions.</p>
+        <Link to="/about"><button className='flex items-center justify-center gap-2 text-2xl text-white bg-gradient-to-r from-purple-900 to-purple-400 py-2 px-4 rounded-lg lg:w-1/3'>More about me? <AiOutlineArrowRight/></button></Link>
       </div>
+    </div>
     </div>
   );
 };
